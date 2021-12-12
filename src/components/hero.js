79d4da136b6e17a6c7f068a7e18447dede417428
yektas <ahmetsercanyektas@gmail.com>
2021-12-12 18:45:50 +0300

    add typewriter, lottie animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 
 import Link from "next/link";
-import { Typewriter } from "react-simple-typewriter";
+//import { Typewriter } from "react-simple-typewriter";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import Lottie from "react-lottie";
+//import * as animationData from "../animations/web-designer.json";
+import * as animationData from "../animations/programming.json";
 
 export const Hero = () => {
   const words = [
@@ -10,22 +15,33 @@ export const Hero = () => {
     "Solidity Developer",
   ];
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+    isClickToPauseDisabled: true,
+  };
+
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <section className="h-screen px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-10 lg:grid-cols-2">
-        <div className="lg:pr-10">
-          <h5 className="mb-4 text-4xl font-extrabold leading-none text-white">
-            Hi there,
-          </h5>
-          <h5 className="mb-4 text-4xl font-extrabold leading-none text-white">
-            I'm{" "}
-            <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-              Sercan Yektas
-            </span>
+        <div className="flex flex-col justify-center font-medium lg:pr-10">
+          <h5 className="mb-4 text-4xl leading-none text-white">Hi there,</h5>
+
+          <h5 className="mb-4 text-4xl leading-none text-white">
+            <Typist cursor={{ show: true, blink: true, element: "_" }}>
+              I'm{" "}
+              <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+                Sercan Yektas
+              </span>
+            </Typist>
           </h5>
           <h5 className="mt-16 text-3xl font-bold text-white">
             <span>
-              <Typewriter
+              {/*  <Typewriter
                 words={words}
                 loop={5}
                 cursor
@@ -33,18 +49,19 @@ export const Hero = () => {
                 typeSpeed={60}
                 deleteSpeed={50}
                 delaySpeed={1500}
-              />
+              /> */}
             </span>
           </h5>
         </div>
-        <div className="p-4 rounded ">
-          <img
+        <div className="pointer-events-none">
+          <Lottie options={defaultOptions} />
+          {/*  <img
             className="object-cover w-full h-56 sm:h-96"
             src="/programming.svg"
             alt=""
-          />
+          /> */}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,51 +7,35 @@ export const Navbar = () => {
       <div className="max-w-screen-xl p-4 mx-auto">
         <div className="flex items-center justify-between space-x-4 lg:space-x-10">
           <div className="flex lg:w-0 lg:flex-1">
-            <span className="h-10 rounded-lg">
+            <span className="rounded-lg">
               <img
-                src="https://res.cloudinary.com/yektas/image/upload/v1584461901/Blog/static/logo/sylogo-dark.png"
+                src="https://res.cloudinary.com/yektas/image/upload/v1584461901/Blog/static/logo/sylogo-light.png"
                 alt="SY Logo"
-                className="cursor-pointer w-28"
+                className="w-32 cursor-pointer"
               />
             </span>
           </div>
 
-          <nav className="hidden space-x-8 text-sm font-medium md:flex">
-            <a className="text-gray-500" href="">
-              About
-            </a>
-            <a className="text-gray-500" href="">
-              Blog
-            </a>
-            <a className="text-gray-500" href="">
-              Projects
-            </a>
-            <a className="text-gray-500" href="">
-              Contact
-            </a>
+          <nav className="hidden space-x-8 font-medium text-white text-md md:flex">
+            <a>About</a>
+            <a>Projects</a>
+            <a href="">Contact Me</a>
           </nav>
 
-          <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
+          <div className="items-center justify-end flex-1 hidden space-x-4 md:flex">
             <a
-              className="px-5 py-2 text-sm font-medium text-gray-500 bg-gray-100 rounded-lg"
               href=""
+              class="inline-block p-0.5 rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
             >
-              {" "}
-              Log in{" "}
-            </a>
-
-            <a
-              className="px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg"
-              href=""
-            >
-              {" "}
-              Sign up{" "}
+              <span class="block px-4 py-2 text-md font-medium text-white bg-gray-900 rounded-full hover:bg-opacity-75">
+                Let's Connect
+              </span>
             </a>
           </div>
 
-          <div className="lg:hidden">
+          <div className="md:hidden">
             <button
-              className="p-2 text-gray-600 bg-gray-100 rounded-lg"
+              className="p-2 text-white bg-gray-900 border-2 border-gray-500 rounded-lg shadow-sm shadow-gray-500/50"
               type="button"
             >
               <span className="sr-only">Open menu</span>
